ceede7b21077bd4358d219d320992d7c504ecf7c
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2022-10-31 12:32:58 +0200

    added Lesson3.java

--- app/src/main/java/com/example/learnbyzantinemusic/MainActivity.java
+++ app/src/main/java/com/example/learnbyzantinemusic/MainActivity.java
@@ -18,9 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
         Button Lesson_1_btn = findViewById(R.id.Lesson_1_id);
         Button Lesson_2_btn = findViewById(R.id.Lesson_2_id);
+        Button Lesson_3_btn = findViewById(R.id.Lesson_3_id);
 
         Lesson_1_btn.setOnClickListener(v -> openLesson1());
         Lesson_2_btn.setOnClickListener(v -> openLesson2());
+        Lesson_3_btn.setOnClickListener(v -> openLesson3());
     }
 
 
@@ -31,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void openLesson2() {
         Intent intent = new Intent(this, Lesson2.class);
+        startActivity(intent);
+    }
+
+    private void openLesson3() {
+        Intent intent = new Intent(this, Lesson3.class);
         startActivity(intent);
     }
 }
