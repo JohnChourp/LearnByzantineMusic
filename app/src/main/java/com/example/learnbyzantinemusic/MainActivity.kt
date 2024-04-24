141fb939502cda87119c26dda08c3913117c496c
JohnChourp <jonhiedarn@gmail.com>
2024-04-24 17:54:42 +0300

    added first lesson

--- app/src/main/java/com/example/learnbyzantinemusic/MainActivity.kt
+++ app/src/main/java/com/example/learnbyzantinemusic/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.learnbyzantinemusic
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_main_activity) // Set the layout resource ID
-        val welcome_text_text_view = findViewById<TextView>(R.id.welcome_text_text_view)
-        val lesson1_text_btn = findViewById<Button>(R.id.lesson1_text_btn)
-        val lesson2_text_btn = findViewById<Button>(R.id.lesson2_text_btn)
+        val lesson1Btn = findViewById<Button>(R.id.lesson1_btn)
+
+        lesson1Btn.setOnClickListener { openLesson1() }
+    }
+
+    private fun openLesson1() {
+        val intent = Intent(this, com.example.learnbyzantinemusic.lessons.lesson1::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/learnbyzantinemusic/lessons/lesson1.kt
@@ -0,0 +1,13 @@
+package com.example.learnbyzantinemusic.lessons
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.learnbyzantinemusic.R
+
+class lesson1 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_lesson_1) // Set the layout resource ID
+
+    }
+}
