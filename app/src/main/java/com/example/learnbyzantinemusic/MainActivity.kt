35f783c1830c21f74eba99130aab6a35c336ca21
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2024-04-29 20:01:19 +0300

    added fifth lesson

--- app/src/main/java/com/example/learnbyzantinemusic/MainActivity.kt
+++ app/src/main/java/com/example/learnbyzantinemusic/MainActivity.kt
@@ -13,11 +13,13 @@ class MainActivity : ComponentActivity() {
         val lesson2Btn = findViewById<Button>(R.id.lesson2_btn)
         val lesson3Btn = findViewById<Button>(R.id.lesson3_btn)
         val lesson4Btn = findViewById<Button>(R.id.lesson4_btn)
+        val lesson5Btn = findViewById<Button>(R.id.lesson5_btn)
 
         lesson1Btn.setOnClickListener { openLesson1() }
         lesson2Btn.setOnClickListener { openLesson2() }
         lesson3Btn.setOnClickListener { openLesson3() }
         lesson4Btn.setOnClickListener { openLesson4() }
+        lesson5Btn.setOnClickListener { openLesson5() }
     }
 
     private fun openLesson1() {
@@ -37,6 +39,11 @@ class MainActivity : ComponentActivity() {
 
     private fun openLesson4() {
         val intent = Intent(this, com.example.learnbyzantinemusic.lessons.Lesson4::class.java)
+        startActivity(intent)
+    }
+
+    private fun openLesson5() {
+        val intent = Intent(this, com.example.learnbyzantinemusic.lessons.Lesson5::class.java)
         startActivity(intent)
     }
 }
