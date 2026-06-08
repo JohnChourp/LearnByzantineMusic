1704f8ed49d4a3a956cf11efc0ba83e2d413d526
JohChourp <jonhiedarn@gmail.com>
2026-06-08 21:15:57 +0300

    release: v1.0.30 - updates in app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.johnchourp.learnbyzantinemusic"
         minSdk = 24
         targetSdk = 34
-        versionCode = 29
-        versionName = "1.0.29"
+        versionCode = 30
+        versionName = "1.0.30"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
