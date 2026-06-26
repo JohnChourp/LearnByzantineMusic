a254e96244e221a021cfc93f32cc43bc4a831ac9
IOANNIS CHOURPOULIADIS <jonhiedarn@gmail.com>
2026-06-26 11:07:26 +0300

    release: v1.5.0 - updates in app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.johnchourp.learnbyzantinemusic"
         minSdk = 24
         targetSdk = 34
-        versionCode = 40
-        versionName = "1.4.1"
+        versionCode = 41
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
